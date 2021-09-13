c0560d2e0cd6a2e67ee76900443f1f60b5dbf001
Camille <camillegomes74@gmail.com>
2021-09-13 09:51:02 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const dragArea = document.querySelector('.frame')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('input')
+const title = document.querySelector('.title')
+
+let file
