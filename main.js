f1fcd5e9e5650c36b197cc9abd78e6cece92f963
Camille <camillegomes74@gmail.com>
2021-09-13 10:12:18 -0300

    Feat: dragover and dragleave event

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const dragArea = document.querySelector('.frame')
+const dropArea = document.getElementById('drop-area')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('input')
 const title = document.querySelector('.title')
 
-let file
+let file;
 
 // If the user click the button, then the input is clicked too
 btn.onclick = ()=>{
     input.click();
 }
+
+// If the user drag the file over dragArea
+dropArea.addEventListener("dragover", ()=>{
+    console.log('file is over dragArea')
+})
+
+// If the user leaves the file from dragArea
+dropArea.addEventListener("dragleave", ()=>{
+    console.log('file is out of dragArea')
+})
