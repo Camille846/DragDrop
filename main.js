2faa6bf18087060ffd1a0ab9339eee3624e04a40
Camille <camillegomes74@gmail.com>
2021-09-13 09:52:55 -0300

    Feat: onclick button event and input click method

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const input = document.querySelector('input')
 const title = document.querySelector('.title')
 
 let file
+
+// If the user click the button, then the input is clicked too
+btn.onclick = ()=>{
+    input.click();
+}
